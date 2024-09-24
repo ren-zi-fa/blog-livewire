af3ae28a0ba13ed7f857877016a8d537f5bc0250
ren-zi-fa <renzifebriandika923@gmail.com>
2024-09-24 11:45:53 +0700

    initial commit

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,5 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
     App\Providers\VoltServiceProvider::class,
+    Spatie\Permission\PermissionServiceProvider::class,
 ];
