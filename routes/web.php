5fa6306b28f17708610978a3c84ae88f78aa05d2
ren-zi-fa <renzifebriandika923@gmail.com>
2024-09-25 12:51:44 +0700

    intial commit

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Livewire\About;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
 Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified','role:admin'])
+    ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
 Route::view('profile', 'profile')
